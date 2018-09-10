a0bf227cd0fd013f374b53b89124ef4602b9d753
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-09-10 14:59:16 -0400

    controller

---
+++ trident-tracklists-api/app/controllers/api/tracklists_controller.rb
@@ -0,0 +1,45 @@
+class Api::TracklistsController < ApplicationController
+before_action :set_surfboard, only: [:show,:edit,:destroy]
+  def index
+    render json: Tracklist.all
+  end
+
+  def create
+    tracklist = Tracklist.new(tracklist_params)
+    if tracklist.save
+      render json: tracklist
+    else
+      render json: {message: surfboard.errors}, status: 400
+  end
+end
+
+def update
+  if @tracklist.update(tracklist_params)
+    render json: @tracklist
+  else
+    render json: {message: surfboard.errors}, status: 400
+  end
+end
+
+def destroy
+  if @tracklist.destroy
+    render status: 204
+  else
+    render json: {message: surfboard.errors}, status: 400
+  end
+end 
+  def show
+    render json: Tracklist.find_by(id: params[:id])
+  end
+
+end
+
+private
+
+def set_tracklist
+  @tracklist = Tracklist.find_by(params[:id])
+end
+
+def tracklist_params
+params.require(:tracklist).permit(:title,:url,:genre,:tracklist)
+end
