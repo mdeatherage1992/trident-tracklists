9aedb2c100d5d18887692c9910052fe8103109e5
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-09-12 13:33:45 -0400

    css styling

--- trident-tracklists-client/src/containers/App.js
+++ trident-tracklists-client/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Tracklists from './Tracklists';
 import './App.css';
+import './Tracklists.css';
 
 const tracklists = [
   {
--- trident-tracklists-client/src/containers/Tracklists.js
+++ trident-tracklists-client/src/containers/Tracklists.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Tracklists.css';
+
 
 const Tracklists = (props) => (
 <div>
 <h1> Tracklists</h1>
 {props.tracklists.map(tracklist =>
-  <div>
+  <div className="tracklistCard">
   <h3>Title: {tracklist.title}</h3>
   <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src={tracklist.url}> </iframe>
   <p>Genre: {tracklist.genre}</p>
