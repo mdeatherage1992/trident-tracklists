f91b16ab2850483d4b8308a04b365785903f1364
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-09-12 15:31:12 -0400

    connection rails and js

--- trident-tracklists-client/src/containers/App.js
+++ trident-tracklists-client/src/containers/App.js
@@ -3,26 +3,44 @@ import Tracklists from './Tracklists';
 import './App.css';
 import './Tracklists.css';
 
-const tracklists = [
-  {
-  title: "new Tracks",
-   url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/497178522&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
-   genre: "Techno",
-   tracklist: "new stuff"
-},
-{
-  title: "old Tracks",
-   url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/493745535&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
-   genre: "Deep",
-   tracklist: "new tunes"
-}
-]
+// const tracklists = [
+//   {
+//   title: "new Tracks",
+//    url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/497178522&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+//    genre: "Techno",
+//    tracklist: "new stuff"
+// },
+// {
+//   title: "old Tracks",
+//    url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/493745535&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+//    genre: "Deep",
+//    tracklist: "new tunes"
+// }
+// ]
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      tracklists: []
+
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/tracklists')
+    .then(response => response.json())
+    .then(tracklists => this.setState({tracklists}))
+  }
   render () {
+    console.log(this.state)
     return (
       <div className="App">
-      <Tracklists tracklists={tracklists} />
+      <Tracklists tracklists={this.state.tracklists} />
       </div>
     );
   }
--- trident-tracklists-client/src/containers/Tracklists.js
+++ trident-tracklists-client/src/containers/Tracklists.js
@@ -8,7 +8,7 @@ const Tracklists = (props) => (
 {props.tracklists.map(tracklist =>
   <div className="tracklistCard">
   <h3>Title: {tracklist.title}</h3>
-  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src={tracklist.url}> </iframe>
+  <iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src={tracklist.url}> </iframe>
   <p>Genre: {tracklist.genre}</p>
   <p>Tracklist: {tracklist.tracklist}</p>
 
